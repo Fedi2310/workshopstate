8f2c75adccf9db382ab1c6b8bbb350243312f791
Fedi2310 <fedi.bey1@gmail.com>
2024-09-03 03:42:13 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  
+    constructor(){
+      super ()
+      this.state={
+        tasks : [
+
+        ],
+        textLive : ""
+
+      }
+    }
+      addTask=()=> this.state.textLive !== ""  &&  this.setState({tasks : [...this.state.tasks,{title : this.state.textLive,isDone : false , id : Math.random()}], textLive :""})
+      deleteTask=(a)=> this.setState({tasks : this.state.tasks.filter((el,i,t)=> el.id !== a)})
+      switchDone=(a)=> this.setState({tasks : this.state.tasks.map((el,i,t)=> el.id===a ? {...el,isDone : true}:el)})
+
+  render() {
+    return(
+      <div className='samir'>
+        <h1>
+          Todo List
+        </h1>
+        {
+          this.state.tasks.map((task,i,t)=>
+          <div className='fedi'>
+            <h3 className={task.isDone && "moez"}>
+              {task.title}
+            </h3>
+            <h4>{task.isDone ? "true" : "false" }</h4>
+            <button onClick={()=>this.switchDone(task.id)}>Done</button>
+            <button onClick={()=>this.deleteTask(task.id)}>Delete</button>
+          </div>
+            )
+        }
+        <br/>
+        <div>
+        <input value={this.state.textLive} type='text' onChange={(e)=>this.setState({textLive: e.target.value})}/>
+        <button onClick={this.addTask}>add</button>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
